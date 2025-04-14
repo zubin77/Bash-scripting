4ce718bc53f4469124f7455176b47b83c9a98b35
zubin77 <69301466+zubin77@users.noreply.github.com>
2025-04-14 22:52:54 +0530

    Update Check_ODDorEVEN_input file .bash

--- Check_ODDorEVEN_input file .bash
+++ Check_ODDorEVEN_input file .bash
@@ -1,3 +1,7 @@
+## Bash Script for check if a number input from standard input is odd or even ##
+## Author: Zubin Patil
+## Created on: 14-04-2025
+## Description: The script is built as part of my personal learning and practice bash scripting.
 #! /bin/bash
 read -p "enter the number: " number
 if [ $(( number % 2 )) -eq 0 ]; then
